8f01a68b2921d44b4b4c7473d46b2ccc2df76821
dmitrikatz23 <43332342+dmitrikatz23@users.noreply.github.com>
2025-01-09 12:59:33 +0100

    starting to construct

--- streamlit/pages/1_RecordActions.py
+++ streamlit/pages/1_RecordActions.py
@@ -1,4 +1,15 @@
-#Page 1: Record Actions
 import streamlit as st
+import csv
+import numpy as np
+import cv2
+import mediapipe as mp
+import time
+import pandas as pd
+import os
+from datetime import datetime
+from streamlit_webrtc import webrtc_streamer
 
-st.title('record actions goes here')
+#set the page configuration
+st.set_page_config(layout = 'wide')
+
+
