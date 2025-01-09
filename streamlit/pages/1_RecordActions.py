8dff74ba1e34a9942e8026d8ca91814a8405a7d7
dmitrikatz23 <43332342+dmitrikatz23@users.noreply.github.com>
2025-01-09 13:06:55 +0100

    check webcam in RecordActions.py

--- streamlit/pages/1_RecordActions.py
+++ streamlit/pages/1_RecordActions.py
@@ -12,4 +12,16 @@ from streamlit_webrtc import webrtc_streamer
 #set the page configuration
 st.set_page_config(layout = 'wide')
 
+#check webcam
+def check_camera_access():
+    cap = cv2.VideoCapture(0) # attempt to access webcam
+    if not cap.isOpened():
+        st.error('webcam is not accessible. please chec browser and system permissions')
+    else:
+        st.success ('webcam is accessible')
+        cap.release()
 
+st.title('webcam permission test')
+
+if st.button ('check Webcam'):
+    check_camera_access()
