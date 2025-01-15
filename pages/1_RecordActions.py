ab5c49b7d63e7931b9f23d138d1ee19d7c586fa1
dmitrikatz23 <43332342+dmitrikatz23@users.noreply.github.com>
2025-01-15 13:03:14 +0100

    updating to match example

--- pages/1_RecordActions.py
+++ pages/1_RecordActions.py
@@ -1,6 +1,21 @@
+import logging
+import queue
+from typing import List, NamedTuple
+import mediapipe as mp
+import av
+import cv2
+import numpy as np
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
+from sample_utils.turn import get_ice_servers
 
-st.title("Live Camera Feed")
 
-webrtc_streamer(key="example")
+# WebRTC Streamer
+webrtc_streamer(
+    key="record_actions",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": get_ice_servers()},
+    media_stream_constraints={"video": True, "audio": False},
+    video_frame_callback=video_frame_callback,
+    async_processing=True,
+)
