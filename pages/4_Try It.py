e4c6f4334af01100820faa80e1ce0a5dd215344a
dmitrikatz23 <43332342+dmitrikatz23@users.noreply.github.com>
2025-03-26 14:02:11 +0100

    moved new try_it to 4, move 5 to backup folder

--- pages/4_Try It.py
+++ pages/4_Try It.py
@@ -416,13 +416,10 @@ os.makedirs(LOCAL_MODEL_DIR, exist_ok=True)
 
 
 
-
-
 # -----------------------------
 # Sidebar: Model Selection
 # -----------------------------
-@st.cache_data
-
+#@st.cache_data
 
 def get_model_encoder_pairs():
     """Retrieve matched model/encoder pairs from Hugging Face."""
